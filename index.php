fd4d03c28583b5e98bcb855d160b45e8a732febe
tom29739 <tom29739@users.noreply.github.com>
2016-09-17 22:02:17 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>PiagetBot</title>
+	<style type="text/css">
+		@font-face {
+			font-family: Lato;
+			src: url(fonts/Lato-Regular.ttf);
+		}
+
+		@font-face {
+			font-family: Lato;
+			src: url(fonts/Lato-Bold.ttf);
+			font-weight: bold;
+		}
+
+		body {
+			font-family: Lato;
+		}
+	</style>
+</head>
+<body>
+	<h1>Nothing much to see here.</h1>
+	Have a random cat fact instead: <strong>
+	<?php echo(json_decode(file_get_contents("http://catfacts-api.appspot.com/api/facts"))->facts[0]); ?></strong>
+</body>
+</html>
